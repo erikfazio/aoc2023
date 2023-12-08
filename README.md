4ad35e268dfbb89418fc181e4893a933b482b2eb
erikfazio <fazioerik@gmail.com>
2023-12-08 19:25:40 +0100

    Day 3 part 1 solved

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%86/yellow)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -76,16 +76,16 @@ Both parts: 12.095ms
 
 ```
 Day 02
-Time part 1: 0.439ms
-Time part 2: 0.791ms
-Both parts: 1.229ms
+Time part 1: 0.443ms
+Time part 2: 0.744ms
+Both parts: 1.187ms
 ```
 
 ```
 Day 03
-Time part 1: -
+Time part 1: 2.37ms
 Time part 2: -
-Both parts: -
+Both parts: 2.37ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 4/50
-Total time: 13.325ms
+Total stars: 5/50
+Total time: 15.651ms
 ```
 
 <!--/RESULTS-->
--- src/day03/index.ts
+++ src/day03/index.ts
@@ -1,11 +1,80 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+const parseInput = (rawInput: string) => {
+  return rawInput.replace(/[\r\n]/gm, "");
+};
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  console.log(input);
+
+  let sum = 0;
+
+  function isNumber(char: string) {
+    return /\d/.test(char);
+  }
+
+  function isSymbol(char: string) {
+    return /[^\d^.]/.test(char);
+  }
+
+  const length = input.length;
+  const depth = Math.sqrt(length);
+  let point;
+  let group = "";
+  let hasAdjacentSymbol = false;
+  let line = -1;
+
+  for (let i = 0; i < length; i++) {
+    point = input[i];
+
+    if (i % depth === 0) {
+      line++;
+    }
+
+    if (isNumber(point)) {
+      if (!hasAdjacentSymbol) {
+        const hasLeftSymbol = i > 0 && isSymbol(input[i - 1]);
+        const hasRightSymbol = i < length - 1 && isSymbol(input[i + 1]);
+        const hasBottomSymbol = line < depth - 1 && isSymbol(input[i + depth]);
+        const hasTopSymbol = line > 0 && isSymbol(input[i - depth]);
+        const hasLeftUpSymbol =
+          i > 0 && line > 0 && isSymbol(input[i - depth - 1]);
+        const hasRightUpSymbol =
+          i < length - 1 && line > 0 && isSymbol(input[i - depth + 1]);
+        const hasLeftDownSymbol =
+          line < depth - 1 && i > 0 && isSymbol(input[i + depth - 1]);
+        const hasRightDownSymbol =
+          line < depth - 1 && i < length - 1 && isSymbol(input[i + depth + 1]);
+
+        if (
+          hasLeftSymbol ||
+          hasRightSymbol ||
+          hasBottomSymbol ||
+          hasTopSymbol ||
+          hasLeftUpSymbol ||
+          hasRightUpSymbol ||
+          hasLeftDownSymbol ||
+          hasRightDownSymbol
+        ) {
+          hasAdjacentSymbol = true;
+        }
+      }
+      group += point;
+    } else {
+      if (group !== "") {
+        if (hasAdjacentSymbol) {
+          console.log(group);
+          sum += parseInt(group);
+        }
+        group = "";
+        hasAdjacentSymbol = false;
+      }
+    }
+  }
+
+  return sum;
 };
 
 const part2 = (rawInput: string) => {
@@ -17,10 +86,20 @@ const part2 = (rawInput: string) => {
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        467..114..
+        ...*......
+        ..35..633.
+        ......#...
+        617*......
+        .....+.58.
+        ..592.....
+        ......755.
+        ...$.*....
+        .664.598..`,
+        expected: 4361,
+      },
     ],
     solution: part1,
   },
